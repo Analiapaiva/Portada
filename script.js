cd42621629a0f4897e6898679cc4ebda43b4db10
Analia Paiva <144301753+Analiapaiva@users.noreply.github.com>
2025-12-22 14:52:10 -0300

    mi primer commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let next = document.querySelector('.next');
+let prev = document.querySelector('.prev');
+
+next.addEventListener('click', function() {
+    let active = document.querySelector('.item')
+    document.querySelectorAll('item')
+    document.querySelector('.slide').appendChild(item[0])
+});
